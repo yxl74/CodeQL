e5fbed6ed096ff0a84e863a51caaa6dc1cdc664f
Eddy Lee <37917160+yxl74@users.noreply.github.com>
2024-09-27 13:18:05 -0700

    Create card.js

---
+++ card.js
@@ -0,0 +1,26 @@
+import * as React from "react"
+
+const Card = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={`rounded-lg border bg-card text-card-foreground shadow-sm ${className}`}
+    {...props}
+  />
+))
+Card.displayName = "Card"
+
+const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={`flex flex-col space-y-1.5 p-6 ${className}`}
+    {...props}
+  />
+))
+CardHeader.displayName = "CardHeader"
+
+const CardContent = React.forwardRef(({ className, ...props }, ref) => (
+  <div ref={ref} className={`p-6 pt-0 ${className}`} {...props} />
+))
+CardContent.displayName = "CardContent"
+
+export { Card, CardHeader, CardContent }
